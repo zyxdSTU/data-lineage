13b92e836e69204322856376308feba43f7b093f
zhouyu <zhouyu@zhejianglab.com>
2023-06-02 15:05:31 +0800

    init

---
+++ src/main/java/org/zjvis/dp/data/lineage/LineageApplication.java
@@ -0,0 +1,13 @@
+package org.zjvis.dp.data.lineage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zjvis/dp/data/lineage/LineageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zjvis.dp.data.lineage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
